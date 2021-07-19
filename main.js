5efa87f42484be5864a9f7d7561ace085572d170
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2021-07-19 12:10:24 +0530

    add docker config

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express()
 
-const PORT = 8080
+const PORT = Number(process.env.PORT) || 4000
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
